996d7789f5867318347b5d435a2102d20871fc4f
Viktor Chynarov <vchynarov@rivierapartners.com>
2015-01-28 16:20:30 -0800

    fixed jshint and code style

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-'use strict'
-// A reporter for jshint
 var notifier = require('node-notifier');
 
 module.exports = {
-    setNotifier: function(notifier) {
-        notifier = notifier
+  setNotifier: function(notifier) {
+    notifier = notifier;
+  },
 
-    },
+  reporter: function(errors) {
+    var errorsPerFile = {};
+    errors.map(function(error) {
+      if (!errorsPerFile[error.file]) {
+        errorsPerFile[error.file] = 0;
+      }
+      errorsPerFile[error.file] += 1;
+    });
 
-    reporter: function(errors) {
-        var errorsPerFile = {}
-        errors.map(function(error) {
-            if(! errorsPerFile[error.file]) {
-                errorsPerFile[error.file] = 0;
-            }
-            errorsPerFile[error.file] += 1;
-
-        })
-
-        for(var file in errorsPerFile) {
-            if(errorsPerFile.hasOwnProperty(file)) {
-                notifier.notify({
-                    title: 'jshint errors',
-                    message: file + ' : ' + errorsPerFile[file] + ' errors.'
-                });
-            }
-        }
-
+    for (var file in errorsPerFile) {
+      if (errorsPerFile.hasOwnProperty(file)) {
+        notifier.notify({
+            title: 'jshint errors',
+            message: file + ' : ' + errorsPerFile[file] + ' errors.'
+        });
+      }
     }
-}
+  }
+};
